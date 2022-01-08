859f5f9c1f308247c3a40cc66d403c3c18d4e3f0
つよし <70616489+pon0204@users.noreply.github.com>
2022-01-08 22:40:49 +0900

    Docker~DB環境構築 (#2)
    * feat: gitignore作成
    
    * feat: Dockerでnestとpostgres起動
    
    * chore: nestjs ライブラリ インストール
    
    * chore: configライブラリインストール
    
    * feat: nesjsDB環境設定

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
