ca1de95090921c794374ca4bd5e0c987228a18b5
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-01-08 22:53:01 +0900

    feat: ユーザーmodule,service,controller作成

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         ...configService.get('database'),
       }),
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
