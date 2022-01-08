17132aff12b2c827dafa8734d6413bb6daf60e51
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-01-08 14:56:40 +0900

    feat: nesjsDB環境設定

---
+++ server/src/config/configuration.ts
@@ -0,0 +1,12 @@
+export default () => ({
+  database: {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DATABASE_SHCEMA,
+    entities: ['dist/**/**.entity{.ts,.js}'],
+    synchronize: true,
+  },
+});
